66f0bd4f70cfd3eeb942db4e7fb779f425c89b9d
Gouri Shankar Kumawat <gskumawat555@gmail.com>
2019-02-25 12:21:20 +0000

    add admin panel

--- index.js
+++ index.js
@@ -57,20 +57,6 @@ app.use(passport.session());
 
 
 passport.use(new LocalStrategy(User.authenticate()));
-// passport.use(new LocalStrategy(
-//     function(username, password, done) {
-//         User.findOne({ username: username }, function(err, user) {
-//             if (err) { return done(err); }
-//             if (!user) {
-//                 return done(null, false, { message: 'Incorrect username.' });
-//             }
-//             if (!user.validPassword(password)) {
-//                 return done(null, false, { message: 'Incorrect password.' });
-//             }
-//             return done(null, user);
-//         });
-//     }
-// ));
 
 passport.serializeUser(function(user, done) {
     done(null, user.id);
